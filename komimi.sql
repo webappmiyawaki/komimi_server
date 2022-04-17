ac17f398c5ba30582f7a2e62eb4af615ecf15fe3
webappmiyawaki <miyawakiwebapp@gmail.com>
2022-04-18 04:53:28 +0900

    sql変更

--- komimi.sql
+++ komimi.sql
@@ -3,6 +3,6 @@ create database komimi_server;
 create table account_info
 (
     customer_id      varchar(100),
-    customer_name    varchar(100),
+    customer_payment    varchar(100),
     customer_balance varchar(100)
 );
